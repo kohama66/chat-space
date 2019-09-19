27f96d34756a9c9fd234c91176030de3b88ed3c8
kohama66 <l.my.me666666@gmail.com>
2019-09-19 13:48:11 +0900

    not search current_user

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,6 +8,7 @@ class GroupsController < ApplicationController
   end
   
   def create
+    
     @group = Group.new(group_params)
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
@@ -30,7 +31,7 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name, { :user_ids => [] }, :group_id,).merge(user_ids: current_user.id)
+    params.require(:group).permit(:name, { :user_ids => [] }, :group_id, )
   end 
 
   def set_group
